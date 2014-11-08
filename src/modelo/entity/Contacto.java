e98c80f3276186376cd16676672629c2aa2cebba
javs <javs@javs>
2014-11-07 22:31:42 -0600

    sss

--- src/modelo/entity/Contacto.java
+++ src/modelo/entity/Contacto.java
@@ -6,14 +6,21 @@
 
 package modelo.entity;
 
-/**
- *
- * @author javs
- */
-public class Contacto {
-    
-    //Atributos
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contactos")
+public class Contacto implements Serializable{
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    
     private String nombre;
     private String apellido;
     private String numero;
